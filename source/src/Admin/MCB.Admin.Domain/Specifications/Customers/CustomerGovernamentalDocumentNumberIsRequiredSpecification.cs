f30d476b3d8439f1aedade7e1e651205dae9136d
Marcelo Castelo <marcelo.castelo@lincros.com>
2019-10-31 15:52:43 -0300

    mais um commit

---
+++ source/src/Admin/MCB.Admin.Domain/Specifications/Customers/CustomerGovernamentalDocumentNumberIsRequiredSpecification.cs
@@ -0,0 +1,21 @@
+﻿using MCB.Admin.Domain.DomainModels;
+using MCB.Admin.Domain.Specifications.Customers.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MCB.Admin.Domain.Specifications.Customers
+{
+    public class CustomerGovernamentalDocumentNumberIsRequiredSpecification
+        : ICustomerGovernamentalDocumentNumberIsRequiredSpecification
+    {
+        public string ErrorCode => "MCB-ADMIN-DOMAIN-CUSTOMERS-2";
+        public string ErrorDefaultDescription => nameof(CustomerGovernamentalDocumentNumberIsRequiredSpecification);
+
+        public Task<bool> IsSatisfiedBy(Customer entity)
+        {
+            return Task.FromResult(!string.IsNullOrWhiteSpace(entity.GovernamentalDocumentNumber));
+        }
+    }
+}
---
+++ source/src/Admin/MCB.Admin.Domain/Specifications/Customers/Interfaces/ICustomerIdIsRequiredSpecification.cs
@@ -0,0 +1,14 @@
+﻿using MCB.Admin.Domain.DomainModels;
+using MCB.Core.Infra.CrossCutting.Patterns.Specification.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MCB.Admin.Domain.Specifications.Customers.Interfaces
+{
+    public interface ICustomerIdIsRequiredSpecification
+        : ISpecification<Customer>
+    {
+
+    }
+}
