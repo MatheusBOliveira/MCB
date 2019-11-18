f2884cefef7cbbe48803683203a55fbc1ef2f776
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2019-11-18 01:10:07 -0300

    mais um commit

---
+++ source/src/Admin/MCB.Admin.Domain/EventHandlers/Customers/CustomerEventHandler.cs
@@ -0,0 +1,13 @@
+﻿using MCB.Core.Infra.CrossCutting.Patterns.CQRS.EventHandlers;
+using MCB.Core.Infra.CrossCutting.Patterns.CQRS.EventHandlers.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MCB.Admin.Domain.EventHandlers.Customers
+{
+    public class CustomerEventHandler
+        : EventHandlerBase
+    {
+    }
+}
---
+++ source/src/Core/MCB.Core.Infra.CrossCutting.Patterns/CQRS/EventHandlers/EventHandlerBase.cs
@@ -0,0 +1,16 @@
+﻿using MCB.Core.Infra.CrossCutting.Patterns.CQRS.EventHandlers.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MCB.Core.Infra.CrossCutting.Patterns.CQRS.EventHandlers
+{
+    public abstract class EventHandlerBase
+        : IDisposable
+    {
+        public void Dispose()
+        {
+            GC.SuppressFinalize(this);
+        }
+    }
+}
