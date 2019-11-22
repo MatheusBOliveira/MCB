3147dcc546b6388e35b3f158626809f785d5e928
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2019-11-22 01:56:05 -0300

    mais um commit

--- source/src/Admin/MCB.Admin.Domain/EventHandlers/Customers/CustomerEventHandler.cs
+++ source/src/Admin/MCB.Admin.Domain/EventHandlers/Customers/CustomerEventHandler.cs
@@ -1,13 +1,28 @@
-﻿using MCB.Core.Infra.CrossCutting.Patterns.CQRS.EventHandlers;
+﻿using MCB.Admin.Domain.Events.Customers;
+using MCB.Core.Infra.CrossCutting.Patterns.CQRS.EventHandlers;
 using MCB.Core.Infra.CrossCutting.Patterns.CQRS.EventHandlers.Interfaces;
+using MCB.Core.Infra.CrossCutting.Patterns.CQRS.Events;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace MCB.Admin.Domain.EventHandlers.Customers
 {
     public class CustomerEventHandler
-        : EventHandlerBase
+        : EventHandlerBase,
+        IEventHandler<CustomerRegistrationSuccessfulEvent>,
+        IEventHandler<CustomerRegistrationFailEvent>
     {
+        public Task<EventReturn> Handle(CustomerRegistrationSuccessfulEvent message, CultureInfo culture, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
+        public Task<EventReturn> Handle(CustomerRegistrationFailEvent message, CultureInfo culture, CancellationToken cancellationToken = default)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
