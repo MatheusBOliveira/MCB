d2ddf8cc7fd01ba6062876a4f291d63bd2af8434
Marcelo Castelo Branco Alves da Silva <marcelo.castelo@outlook.com>
2019-11-22 03:31:16 -0300

    mais um commit

---
+++ source/src/Core/MCB.Core.Infra.CrossCutting.Patterns/Retry/Enums/BackoffAlgorithmTypeEnum.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MCB.Core.Infra.CrossCutting.Patterns.Retry.Enums
+{
+    public enum BackoffAlgorithmTypeEnum
+    {
+        Decorr = 1,
+        EqualJitter = 2,
+        FullJitter = 3,
+        None = 4
+    }
+}
---
+++ source/src/Core/MCB.Core.Infra.CrossCutting.Patterns/Retry/RetryPolicy.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MCB.Core.Infra.CrossCutting.Patterns.Retry
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <remarks>
+    /// see https://medium.com/@trongdan_tran/circuit-breaker-and-retry-64830e71d0f6
+    /// </remarks>
+    public class RetryPolicy
+    {
+        private readonly TimeSpan _timeout;
+        private readonly int _maxAttempts;
+
+        public TimeSpan Timeout
+        {
+            get
+            {
+                return _timeout;
+            }
+        }
+        public int MaxAttempts
+        {
+            get
+            {
+                return _maxAttempts;
+            }
+        }
+
+        public RetryPolicy()
+        {
+
+        }
+    }
+}
